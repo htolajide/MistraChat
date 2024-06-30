ac50f14c9310ca270aed2596312412aa67dba25f
Hammed Taofeek <htolajide@yahoo.com>
2024-06-30 13:43:15 +0100

    emdeddings-created

--- document.js
+++ document.js
@@ -4,16 +4,39 @@
 //     console.info(text);
 // }
 import fs from 'fs';
+import { RecursiveCharacterTextSplitter} from 'langchain/text_splitter';
+import MistralClient from '@mistralai/mistralai';
+import dotenv from 'dotenv';
 
-async function split_document () {
-    fs.readFile('handbook.txt', 'utf8', (err, data) => {
-    if (err) {
+dotenv.config();
+
+const client = new MistralClient(process.env.Mistral_API_KEY);
+
+ async function split_document (path) {
+    //const content = [];
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const spliter = new RecursiveCharacterTextSplitter({
+            chunkSize: 250,
+            chunkOverlap: 40
+        })
+        const output = await spliter.createDocuments([data]);
+        const content = output.map((item) => item.pageContent);
+        //console.info(output);
+        return content;
+        //console.log(data);
+      } catch (err) {
         console.error(err);
-        return;
-    }
-    console.info(data);
-    });
+      }
 }
 
-
-split_document();
+const result = await split_document('handbook.txt');
+//console.log(result);
+const exampleChunk = 'Android makes a distinction between implicit and explicit broadcast actions. It defines' +
+    ' an explicit broadcast as something that target just one application, no matter how many ' +
+  'other apps are listening for it';
+const embeddingResponse = await client.embeddings({
+    model: "mistral-embed",
+    input: exampleChunk
+})
+console.info(embeddingResponse)
